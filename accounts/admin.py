bf595f16fc069624c4991af52d14fbddbde34393
cheshta1828 <cheshtaabatra1828@gmail.com>
2023-11-22 20:10:30 +0530

    superuser fixed

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from .models import customUser
 # Register your models here.
 admin.site.register(customUser)
+
+
+
+
+
+
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,36 +1,83 @@
+# from django.db import models
+# # inport user
+# from django.contrib.auth.models import AbstractUser as User , BaseUserManager
+# from django.contrib.auth.hashers import make_password
+# #modifying user model
+
+
+# class customUser(User):
+#     otp = models.CharField(max_length=4 ,null=True, blank=True)
+#     otp_valid_till = models.DateTimeField(null=True, blank=True)
+#     email = models.EmailField(unique=True)
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+#     def save(self, *args, **kwargs):
+#         self.password = make_password("password")
+#         print(self.password)
+#         if self.is_superuser == False:
+#             if "@msijanakpuri.com" not in self.email or len(self.email) == len("@msijanakpuri.com"):
+#                 print(self.email)
+#                 raise Exception("Email is not valid")
+#             super().save(*args, **kwargs)
+#         else:
+#             super().save(*args, **kwargs) 
+
+#     # def create_superuser(self, email, password=None, **extra_fields):
+#     #     extra_fields.setdefault('is_staff', True)
+#     #     extra_fields.setdefault('is_superuser', True)
+
+#     #     if extra_fields.get('is_staff') is not True:
+#     #         raise ValueError('Superuser must have is_staff=True.')
+#     #     if extra_fields.get('is_superuser') is not True:
+#     #         raise ValueError('Superuser must have is_superuser=True.')
+
+#     #     return self.create_user(email, password, **extra_fields)
+
 from django.db import models
-# inport user
-from django.contrib.auth.models import AbstractUser as User , BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.auth.hashers import make_password
-#modifying user model
+from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 
-class customUser(User):
-    otp = models.CharField(max_length=4 ,null=True, blank=True)
+class customUser(AbstractUser):
+    otp = models.CharField(max_length=4, null=True, blank=True)
     otp_valid_till = models.DateTimeField(null=True, blank=True)
     email = models.EmailField(unique=True)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
+
+    objects = CustomUserManager()
+
     def save(self, *args, **kwargs):
-        self.password = make_password("password")
-        print(self.password)
-        if self.is_superuser == False:
+        if not self.is_superuser:
+            # If it's not a superuser, set the password using make_password
+            self.password = make_password("password")
+            # Your custom validation logic for non-superusers
             if "@msijanakpuri.com" not in self.email or len(self.email) == len("@msijanakpuri.com"):
-                print(self.email)
                 raise Exception("Email is not valid")
-            super().save(*args, **kwargs)
-        else:
-            super().save(*args, **kwargs) 
 
-    # def create_superuser(self, email, password=None, **extra_fields):
-    #     extra_fields.setdefault('is_staff', True)
-    #     extra_fields.setdefault('is_superuser', True)
-
-    #     if extra_fields.get('is_staff') is not True:
-    #         raise ValueError('Superuser must have is_staff=True.')
-    #     if extra_fields.get('is_superuser') is not True:
-    #         raise ValueError('Superuser must have is_superuser=True.')
-
-    #     return self.create_user(email, password, **extra_fields)
+        super().save(*args, **kwargs)
+        
 
 
