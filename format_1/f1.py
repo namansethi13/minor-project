ba7dd4033cf47ac4acd10637c961531a16d6d3d1
ChromaBeast <sheershjaiswal@gmail.com>
2023-11-28 23:06:06 +0530

    Update f1.py

--- format_1/f1.py
+++ format_1/f1.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from docx import Document
 from docx.shared import Inches
@@ -30,7 +29,7 @@ class Format_1:
             self.subject_name_mapping.update({key + ".1": key+" "+value + " (External)"})
             self.subject_name_mapping.update({key + ".2": key+" "+value + " (Total)"})
 
-    def read_data(self,subjects,students,):
+    def read_data(self,subjects,students):
 
         col_names = ['S.No', 'Enrollment Number', 'Name', 'Sec',]
         for i in range(len(subjects)):
@@ -55,17 +54,13 @@ class Format_1:
         self.excel_df = self.excel_df[self.excel_df['Sec'] == sec]
         self.excel_df = self.excel_df.drop(columns=['Sec'])
 
-    def write_to_excel(self):
-        self.excel_file_path = 'output2.xlsx'
-        self.excel_df.to_excel(self.excel_file_path, index=False)
-        print("Excel file created at: ", self.excel_file_path)
-
     def read_from_filtered_excel(self, course_name,subject_code):
         
         self.filtered_df = pd.DataFrame()  # Initialize filtered_df as an empty dataframe
 
     # Assuming the column you want to work with is named 'YourColumnName'
         column_names = list()
+        sum_a=sum_b=0 
         for i in range(len(subject_code)):
             column_names.append(subject_code[i]+" "+self.all_subjects[subject_code[i]]+" (Total)")
         
@@ -92,12 +87,14 @@ class Format_1:
                 self.excel_df[column_name] <= 89)].shape[0]
             countA3 = self.excel_df[(self.excel_df[column_name] >= 60) & (
                 self.excel_df[column_name] <= 74)].shape[0]
+            sum_a+=countA1+countA2+countA3
             countB1 = self.excel_df[(self.excel_df[column_name] >= 50) & (
                 self.excel_df[column_name] <= 59)].shape[0]
             countB2 = self.excel_df[(self.excel_df[column_name] >= 40) & (
                 self.excel_df[column_name] <= 49)].shape[0]
             countB3 = self.excel_df[(self.excel_df[column_name] >= 1) & (
                 self.excel_df[column_name] <= 39)].shape[0]
+            sum_b+=countB1+countB2+countB3
             self.filtered_df.at[i, '>=90%'] = str(
                 f"{countA1}\n({countA1 / total_students * 100:.2f})")
             self.filtered_df.at[i,
@@ -113,6 +110,49 @@ class Format_1:
             self.filtered_df.at[i,'C=B-A'] = f'{(countB1+countB2+countB3-countA1):.0f}'
             self.filtered_df.at[i,
                                 'Highest Marks'] = f'{self.excel_df[column_name].max():.0f}'
+            print(self.filtered_df)
+        
+
+        second_last_row = pd.Series({
+    "S.No": "",
+    "Paper Code": "",
+    "Subjects Taught": "Total Students & Pass %",
+    "Students Appeared": (self.filtered_df["Students Appeared"].astype(int)).sum(),
+    "Passed": (self.filtered_df["Passed"].astype(int)).sum(),
+    "Pass %": f'{((self.filtered_df["Passed"].astype(int)).sum() / (self.filtered_df["Students Appeared"].astype(int)).sum()) * 100:.2f}',
+    '>=90%': f"No. of Students and average %age above 60% {sum_a} ({sum_a / (self.filtered_df['Students Appeared'].astype(int)).sum() * 100:.2f}))",
+    "89.99 - 75%": "",
+    "74.99 - 60%": "",
+    "59.99 - 50%": f"No. of Students and average %age below 60% {sum_b} ({sum_b / (self.filtered_df['Students Appeared'].astype(int)).sum() * 100:.2f}))",
+    "49.99-40%": "",
+    "<40%": "",
+    "C=B-A": "",
+    "Highest Marks": "",
+})
+        last_row = pd.Series({
+    "S.No": "",
+    "Paper Code": """*Relaxation of 2% in addition to C who are regular and punctual during teaching
+     days from 2Aug-9Nov (availed upto 6 leave) excluding the time period of mid-term
+     exams from 8Oct.-13Oct.18*
+     This has been shifted to pt. 4 of Faculty Performance criterion.""",
+    "Subjects Taught": "",
+    "Students Appeared": "",
+    "Passed": "",
+    "Pass %": "",
+    '>=90%': "",
+    "89.99 - 75%": "",
+    "74.99 - 60%": "",
+    "59.99 - 50%": "",
+    "49.99-40%": "",
+    "<40%": "",
+    "C=B-A": "",
+    "Highest Marks": "",
+})
+
+# Append the new row to the DataFrame
+        self.filtered_df = self.filtered_df._append(second_last_row, ignore_index=True)
+        self.filtered_df = self.filtered_df._append(last_row, ignore_index=True)
+        print(self.filtered_df)
 
 
     def write_to_doc(self):
@@ -171,58 +211,34 @@ class Format_1:
             cell.paragraphs[0].runs[0].font.size = Pt(10)
 
 # Add the rest of the data frame with bold, Times New Roman, and text size 10
+
         for i in range(excel_data_df.shape[0]):
             for j in range(excel_data_df.shape[-1]):
-                cell = table.cell(i + 1, j)
+                cell = table.cell(i+1, j)
                 cell.text = str(excel_data_df.values[i, j])
-                cell.paragraphs[0].runs[0].bold = True
                 cell.paragraphs[0].runs[0].font.name = 'Times New Roman'
                 cell.paragraphs[0].runs[0].font.size = Pt(10)
-        #add a row at the end 		Total Students & Pass %	118	117	99.15%	No. of students & average % above 60% 82 (69.49%)	No. of students & average % below 60% 34 (28.81%)	
+                cell.paragraphs[0].runs[0].bold = True 
+                cell.paragraphs[0].runs[0].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        def merge_cells(table, start_row, start_col, end_row, end_col):
+            for row in range(start_row, end_row + 1):
+                for col in range(start_col, end_col + 1):
+                    cell = table.cell(row, col)
+                    if row == start_row and col == start_col:
+                        continue  # Skip the first cell, as it will be the merged cell
+                    cell.text = cell.text.strip()
+                    
 
-        # table.add_row()
-        # cell = table.cell(excel_data_df.shape[0]+1, 0)
-        # cell.text = "Total Students & Pass %"
-        # cell.paragraphs[0].runs[0].bold = True
-        # cell.paragraphs[0].runs[0].font.name = 'Times New Roman'
-        # cell.paragraphs[0].runs[0].font.size = Pt(10)
-        # cell = table.cell(excel_data_df.shape[0]+1, 1)
-        # appeared_text = f'{excel_data_df["Students Appeared"].sum():.0f}'
-        # cell.text = appeared_text
-        # cell.paragraphs[0].runs[0].bold = True
-        # cell.paragraphs[0].runs[0].font.name = 'Times New Roman'
-        # cell.paragraphs[0].runs[0].font.size = Pt(10)
-        # cell = table.cell(excel_data_df.shape[0]+1, 2)
-        # cell.text = f'{excel_data_df["Passed"].sum():.0f}'
-        # cell.paragraphs[0].runs[0].bold = True
-        # cell.paragraphs[0].runs[0].font.name = 'Times New Roman'
-        # cell.paragraphs[0].runs[0].font.size = Pt(10)
-        # cell = table.cell(excel_data_df.shape[0]+1, 3)
-        # cell.text = f'{(excel_data_df["Passed"].sum()/excel_data_df["Students Appeared"].sum())*100:.2f}%'
-        # cell.paragraphs[0].runs[0].bold = True
-        # cell.paragraphs[0].runs[0].font.name = 'Times New Roman'
-        # cell.paragraphs[0].runs[0].font.size = Pt(10)
-        # cell = table.cell(excel_data_df.shape[0]+1, 4)
-        # cell.text = "No. of students & average % above 60%"
-        # cell.paragraphs[0].runs[0].bold = True
-        # cell.paragraphs[0].runs[0].font.name = 'Times New Roman'
-        # cell.paragraphs[0].runs[0].font.size = Pt(10)
-        # cell = table.cell(excel_data_df.shape[0]+1, 5)
-        # cell.text = f'{excel_data_df["Students Appeared"].sum()-excel_data_df[excel_data_df["<40%"]!="0"].shape[0]:.0f} ({(excel_data_df["Students Appeared"].sum()-excel_data_df[excel_data_df["<40%"]!="0"].shape[0])/excel_data_df["Students Appeared"].sum()*100:.2f}%)'
-        # cell.paragraphs[0].runs[0].bold = True
-        # cell.paragraphs[0].runs[0].font.name = 'Times New Roman'
-        # cell.paragraphs[0].runs[0].font.size = Pt(10)
-        # cell = table.cell(excel_data_df.shape[0]+1, 6)
-        # cell.text = "No. of students & average % below 60%"
-        # cell.paragraphs[0].runs[0].bold = True
-        # cell.paragraphs[0].runs[0].font.name = 'Times New Roman'
-        # cell.paragraphs[0].runs[0].font.size = Pt(10)
-        # cell = table.cell(excel_data_df.shape[0]+1, 7)
-        # cell.text = f'{excel_data_df[excel_data_df["<40%"]!="0"].shape[0]:.0f} ({excel_data_df[excel_data_df["<40%"]!="0"].shape[0]/excel_data_df["Students Appeared"].sum()*100:.2f}%)'
-        # cell.paragraphs[0].runs[0].bold = True
-        # cell.paragraphs[0].runs[0].font.name = 'Times New Roman'
-        # cell.paragraphs[0].runs[0].font.size = Pt(10)
-        
+    # Merge the cells
+            table.cell(start_row, start_col).merge(table.cell(end_row, end_col))
+
+
+
+# Specify the range of cells to merge (indices are zero-based)
+        merge_cells(table, start_row=len(self.filtered_df)-1, start_col=6, end_row=len(self.filtered_df)-1, end_col=8)  # Merging columns 7 to 9
+        merge_cells(table, start_row=len(self.filtered_df)-1, start_col=9, end_row=len(self.filtered_df)-1, end_col=12)  # Merging columns 10 to 13
+        merge_cells(table, start_row=len(self.filtered_df), start_col=9, end_row=len(self.filtered_df), end_col=12)  # Merging columns 10 to 13
+        merge_cells(table, start_row=len(self.filtered_df), start_col=1, end_row=len(self.filtered_df), end_col=8)  # Merging columns 14 to 15
 # Add the footer
         footer_lines = [
             "",
@@ -253,7 +269,7 @@ Assistant Professor 		Convenor-Result Analysis Committee         	HOD-BCA[M]"""
 
 
 #driver code
-subjects=['020102', '020104', '020106','020108', '020110', '020136','020172','020174','020176']
+subject_codes=['020102', '020104', '020106','020108', '020110', '020136','020172','020174','020176']
 all_subjects={'020102': 'Applied Maths',
               '020104': 'Web Based Programming',
               '020106': 'Data Structures & Algorithm Using C',
@@ -265,7 +281,7 @@ all_subjects={'020102': 'Applied Maths',
               '020176': 'Practical- VI DBMS Lab'}
 f1 = Format_1("output.xlsx",all_subjects)
 needed_subjects = ['020102', '020104', ]
-f1.read_data(subjects, 111)
-f1.filter_data(subjects, "B")
+f1.read_data(subject_codes, 111)
+f1.filter_data(subject_codes, "B")
 f1.read_from_filtered_excel("BCA",needed_subjects)
 f1.write_to_doc()
