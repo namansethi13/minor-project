07ddd45704512236dbf06c3cb38cfbcdcae9e4d6
ChromaBeast <sheershjaiswal@gmail.com>
2023-11-29 08:37:14 +0530

    fixed column names and styling in format 2

--- format_1/f2.py
+++ format_1/f2.py
@@ -2,12 +2,12 @@ import pandas as pd
 from docx import Document
 from docx.shared import Inches
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from docx.shared import Pt,RGBColor
+from docx.shared import Pt, RGBColor
 
 
 class Format_2:
 
-    def __init__(self, input_file,all_subjects):
+    def __init__(self, input_file, all_subjects):
         self.input_file = input_file
         self.all_subjects = all_subjects
         self.df = None
@@ -23,19 +23,22 @@ class Format_2:
                                      'Total Marks': 'Total Marks',
                                      'CGPA%': 'CGPA%',
                                      'Reappear': 'Reappear',
-                                     'Absent': 'Absent',}
+                                     'Absent': 'Absent', }
         for key, value in self.all_subjects.items():
-            self.subject_name_mapping.update({key: key+" "+value+" (Internal)"})
-            self.subject_name_mapping.update({key + ".1": key+" "+value + " (External)"})
-            self.subject_name_mapping.update({key + ".2": key+" "+value + " (Total)"})
+            self.subject_name_mapping.update(
+                {key: key+" "+value+" (Internal)"})
+            self.subject_name_mapping.update(
+                {key + ".1": key+" "+value + " (External)"})
+            self.subject_name_mapping.update(
+                {key + ".2": key+" "+value + " (Total)"})
 
-    def read_data(self,subjects, sec):
+    def read_data(self, subjects, sec):
 
         col_names = ['S.No', 'Enrollment Number', 'Name', 'Sec',]
         for i in range(len(subjects)):
             col_names += [subjects[i], subjects[i]+".1", subjects[i]+".2"]
         col_names += [
-                     'Total Marks', 'CGPA%',  'Reappear', 'Absent',]
+            'Total Marks', 'CGPA%',  'Reappear', 'Absent',]
         self.df = pd.read_excel(
             self.input_file, skiprows=6, names=col_names)
         self.df.columns = [self.subject_name_mapping.get(
@@ -52,25 +55,22 @@ class Format_2:
         self.excel_df = self.excel_df[self.excel_df['Sec'] == sec]
         self.excel_df = self.excel_df.drop(columns=['Sec'])
 
+    def read_from_filtered_excel(self, course_name, subject_code, teacher_list):
 
-    def read_from_filtered_excel(self, course_name,subject_code):
-        
         self.filtered_df = pd.DataFrame()  # Initialize filtered_df as an empty dataframe
 
     # Assuming the column you want to work with is named 'YourColumnName'
         column_names = list()
-        sum_a=sum_b=0 
+        sum_a = sum_b = 0
         for i in range(len(subject_code)):
-            column_names.append(subject_code[i]+" "+self.all_subjects[subject_code[i]]+" (Total)")
-        
-                        
-        
+            column_names.append(
+                subject_code[i]+" "+self.all_subjects[subject_code[i]]+" (Total)")
+
         for i, column_name in enumerate(column_names):
             self.filtered_df.at[i, 'S.No'] = f'{i+1:.0f}'
-            self.filtered_df.at[i, 'Faculty Name'] = str(
-                course_name + str(column_name[3:6]))
+            self.filtered_df.at[i, 'Faculty Name'] = teacher_list[i]
             self.filtered_df.at[i, 'Subject'] = str(
-                column_name[7:-8])
+                column_name[7:-8])+f' ({column_name[:6]})'
 
             total_students = self.excel_df[self.excel_df[column_name]
                                            != 0].shape[0]
@@ -86,14 +86,14 @@ class Format_2:
                 self.excel_df[column_name] <= 89)].shape[0]
             countA3 = self.excel_df[(self.excel_df[column_name] >= 60) & (
                 self.excel_df[column_name] <= 74)].shape[0]
-            sum_a+=countA1+countA2+countA3
+            sum_a += countA1+countA2+countA3
             countB1 = self.excel_df[(self.excel_df[column_name] >= 50) & (
                 self.excel_df[column_name] <= 59)].shape[0]
             countB2 = self.excel_df[(self.excel_df[column_name] >= 40) & (
                 self.excel_df[column_name] <= 49)].shape[0]
             countB3 = self.excel_df[(self.excel_df[column_name] >= 1) & (
                 self.excel_df[column_name] <= 39)].shape[0]
-            sum_b+=countB1+countB2+countB3
+            sum_b += countB1+countB2+countB3
             self.filtered_df.at[i, '90% & Above'] = str(
                 f"{countA1}\n({countA1 / total_students * 100:.2f})")
             self.filtered_df.at[i,
@@ -109,44 +109,45 @@ class Format_2:
             self.filtered_df.at[i,
                                 'Highest Marks'] = f'{self.excel_df[column_name].max():.0f}'
             print(self.filtered_df)
-        
 
         second_last_row = pd.Series({
-    "S.No": "",
-    "Faculty Name": "",
-    "Subject": "Total Subjects",
-    "Appeared": "Total Students Appeared",
-    "Passed": '',
-    "Pass %": '',
-    '90% & Above': 'No. & % of Students above 60%',
-    "75% to less than 90%": "",
-    "60% to less than 75%": "",
-    "50% to less than 60%": 'No. & % of Students below 60%',
-    "40% to less than50%": "",
-    "Below 40%": "",
-    
-    "Highest Marks": "",
-})
+            "S.No": "",
+            "Faculty Name": "",
+            "Subject": "Total Subjects",
+            "Appeared": "Total Students Appeared",
+            "Passed": '',
+            "Pass %": '',
+            '90% & Above': 'No. & % of Students above 60%',
+            "75% to less than 90%": "",
+            "60% to less than 75%": "",
+            "50% to less than 60%": 'No. & % of Students below 60%',
+            "40% to less than50%": "",
+            "Below 40%": "",
+
+            "Highest Marks": "",
+        })
         last_row = pd.Series({
-    "S.No": "",
-    "Faculty Name": '',
-    "Subject": len(subject_code),
-    "Appeared": self.filtered_df['Appeared'].max(),
-    "Passed": "",
-    "Pass %": "",
-    '90% & Above': f"{sum_a/len(subject_code):.2f} ({sum_a / (self.filtered_df['Appeared'].astype(int)).sum() * 100:.2f})",
-    "75% to less than 90%": "",
-    "60% to less than 75%": "",
-    "50% to less than 60%": f"{sum_b/len(subject_code):.2f} ({sum_b / (self.filtered_df['Appeared'].astype(int)).sum() * 100:.2f})",
-    "40% to less than50%": "",
-    "Below 40%": "",
-    
-    "Highest Marks": "",
-})
+            "S.No": "",
+            "Faculty Name": '',
+            "Subject": len(subject_code),
+            "Appeared": self.filtered_df['Appeared'].max(),
+            "Passed": "",
+            "Pass %": "",
+            '90% & Above': f"{sum_a/len(subject_code):.2f} ({sum_a / (self.filtered_df['Appeared'].astype(int)).sum() * 100:.2f})",
+            "75% to less than 90%": "",
+            "60% to less than 75%": "",
+            "50% to less than 60%": f"{sum_b/len(subject_code):.2f} ({sum_b / (self.filtered_df['Appeared'].astype(int)).sum() * 100:.2f})",
+            "40% to less than50%": "",
+            "Below 40%": "",
+
+            "Highest Marks": "",
+        })
 
 # Append the new row to the DataFrame
-        self.filtered_df = self.filtered_df._append(second_last_row, ignore_index=True)
-        self.filtered_df = self.filtered_df._append(last_row, ignore_index=True)
+        self.filtered_df = self.filtered_df._append(
+            second_last_row, ignore_index=True)
+        self.filtered_df = self.filtered_df._append(
+            last_row, ignore_index=True)
         print(self.filtered_df)
 
     def write_to_doc(self):
@@ -154,11 +155,15 @@ class Format_2:
         excel_data_df = self.filtered_df
         doc = Document()
         for section in doc.sections:
-            
-            section.left_margin =section.right_margin= Inches(0.4)
-            
+
+            section.left_margin = section.right_margin = Inches(0.4)
+
             section.top_margin = section.bottom_margin = Inches(0)
-            
+        bold_text = ['Total Subjects','Total Students Appeared',
+
+                     "No. & % of Students above 60%",
+
+                     "No. & % of Students below 60%"]
 # Add lines to the header
         header_lines = [
             "",
@@ -189,11 +194,13 @@ class Format_2:
             for run in paragraph.runs:
                 font = run.font
                 font.name = 'Times New Roman'
-                font.size = Pt(20) if paragraph.text == 'MAHARAJA SURAJMAL INSTITUTE' else Pt(14)
+                font.size = Pt(
+                    20) if paragraph.text == 'MAHARAJA SURAJMAL INSTITUTE' else Pt(14)
                 font.bold = True
+                if paragraph.text == 'Class-wise Result Analysis':
+                    font.underline = True
                 font.color.rgb = RGBColor(0, 0, 0)  # RGB values for black
-        
-            
+
         doc.add_table(
             excel_data_df.shape[0]+1, excel_data_df.shape[1], style='Table Grid')
         table = doc.tables[0]
@@ -213,8 +220,10 @@ class Format_2:
                 cell.text = str(excel_data_df.values[i, j])
                 cell.paragraphs[0].runs[0].font.name = 'Times New Roman'
                 cell.paragraphs[0].runs[0].font.size = Pt(10)
-                cell.paragraphs[0].runs[0].bold = True 
+                if cell.text in bold_text:
+                    cell.paragraphs[0].runs[0].bold = True
                 cell.paragraphs[0].runs[0].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
         def merge_cells(table, start_row, start_col, end_row, end_col):
             for row in range(start_row, end_row + 1):
                 for col in range(start_col, end_col + 1):
@@ -222,25 +231,30 @@ class Format_2:
                     if row == start_row and col == start_col:
                         continue  # Skip the first cell, as it will be the merged cell
                     cell.text = cell.text.strip()
-                    
 
     # Merge the cells
-            table.cell(start_row, start_col).merge(table.cell(end_row, end_col))
-
+            table.cell(start_row, start_col).merge(
+                table.cell(end_row, end_col))
 
 
 # Specify the range of cells to merge (indices are zero-based)
-        merge_cells(table, start_row=len(self.filtered_df)-1, start_col=3, end_row=len(self.filtered_df)-1, end_col=5)  # Merging columns 7 to 9
-        merge_cells(table, start_row=len(self.filtered_df)-1, start_col=6, end_row=len(self.filtered_df)-1, end_col=8)
-        merge_cells(table, start_row=len(self.filtered_df)-1, start_col=9, end_row=len(self.filtered_df)-1, end_col=11)
-        merge_cells(table, start_row=len(self.filtered_df), start_col=3, end_row=len(self.filtered_df), end_col=5)  # Merging columns 7 to 9
-        merge_cells(table, start_row=len(self.filtered_df), start_col=6, end_row=len(self.filtered_df), end_col=8)
-        merge_cells(table, start_row=len(self.filtered_df), start_col=9, end_row=len(self.filtered_df), end_col=11)
-        
+        merge_cells(table, start_row=len(self.filtered_df)-1, start_col=3,
+                    end_row=len(self.filtered_df)-1, end_col=5)  # Merging columns 7 to 9
+        merge_cells(table, start_row=len(self.filtered_df)-1,
+                    start_col=6, end_row=len(self.filtered_df)-1, end_col=8)
+        merge_cells(table, start_row=len(self.filtered_df)-1,
+                    start_col=9, end_row=len(self.filtered_df)-1, end_col=11)
+        merge_cells(table, start_row=len(self.filtered_df), start_col=3, end_row=len(
+            self.filtered_df), end_col=5)  # Merging columns 7 to 9
+        merge_cells(table, start_row=len(self.filtered_df),
+                    start_col=6, end_row=len(self.filtered_df), end_col=8)
+        merge_cells(table, start_row=len(self.filtered_df),
+                    start_col=9, end_row=len(self.filtered_df), end_col=11)
+
 # Add the footer
         footer_lines = ["",
-            "         Class Coordinator	                  Result Analysis Committee		  HOD, BBA (M)",
-            "          Ms. XYZ	                                           Dr. ABC	                                      Dr. DEF",]
+                        "         Class Coordinator	                  Result Analysis Committee		  HOD, BBA (M)",
+                        "          Ms. XYZ	                                           Dr. ABC	                                      Dr. DEF",]
         for line in footer_lines:
             paragraph = doc.add_paragraph(line)
             paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
@@ -250,29 +264,29 @@ class Format_2:
                 font = run.font
                 font.name = 'Times New Roman'
                 font.size = Pt(14)
-                if line != '“I do hereby solemnly affirm and declare that the facts stated in the above result are true to the best of my knowledge and belief”':
-                    font.bold = True
+                font.bold = True
 
                 font.color.rgb = RGBColor(0, 0, 0)  # RGB values for black
-            
 
-        
         doc.save(self.word_file_path)
 
 
-#driver code
-subject_codes=['020102', '020104', '020106','020108', '020110', '020136','020172','020174','020176']
-all_subjects={'020102': 'Applied Maths',
-              '020104': 'Web Based Programming',
-              '020106': 'Data Structures & Algorithm Using C',
-              '020108': 'DBMS',
-              '020110': 'EVS',
-              '020136': 'SAUE',
-              '020172': 'Practical IV-WBP Lab',
-              '020174': 'Practical- V DS Lab',
-              '020176': 'Practical- VI DBMS Lab'}
-f1 = Format_2("sem2.xlsx",all_subjects)
-needed_subjects = ['020102', '020106','020108' ]
+# driver code
+subject_codes = ['020102', '020104', '020106', '020108',
+                 '020110', '020136', '020172', '020174', '020176']
+all_subjects = {'020102': 'Applied Maths',
+                '020104': 'Web Based Programming',
+                '020106': 'Data Structures & Algorithm Using C',
+                '020108': 'DBMS',
+                '020110': 'EVS',
+                '020136': 'SAUE',
+                '020172': 'Practical IV-WBP Lab',
+                '020174': 'Practical- V DS Lab',
+                '020176': 'Practical- VI DBMS Lab'}
+
+teacher_list = ['Dr. ABC', 'Dr. DEF', 'Dr. GHI', 'Dr. JKL',
+                'Dr. MNO', 'Dr. PQR', 'Dr. STU', 'Dr. VWX', 'Dr. YZ']
+f1 = Format_2("sem2.xlsx", all_subjects)
 f1.read_data(subject_codes, "B",)
-f1.read_from_filtered_excel("BCA",subject_codes)
+f1.read_from_filtered_excel("BCA", subject_codes, teacher_list)
 f1.write_to_doc()
