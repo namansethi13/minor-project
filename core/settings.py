f7671e38301e880b60a048d34a715ec09cd3dcf7
stinger13 <93597591+stinger13@users.noreply.github.com>
2023-12-07 02:58:19 +0530

    centralized database

--- core/settings.py
+++ core/settings.py
@@ -86,13 +86,25 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': getenv("NAME_AND_USER_ES"),
+    'USER': getenv("NAME_AND_USER_ES"),
+    'PASSWORD': getenv("PASSWORD_ES"),
+    # ↓ HOST instead of HOSTS
+    'HOST': getenv("HOST_ES"),
+    'PORT': 5432
+  }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
