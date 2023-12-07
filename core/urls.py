9774f515fe9f8871fb299159535e0986059b9ca5
stinger13 <93597591+stinger13@users.noreply.github.com>
2023-12-07 21:13:51 +0530

    settings changed

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,7 +27,5 @@ urlpatterns = [
     path('', lambda request: redirect('/results/convert'), name='home_redirect'),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-from django.conf import settings
-from django.conf.urls.static import static
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
