d6b068c3991cbf6dd9e537da3ec8e1bc04e21e48
stinger13 <93597591+stinger13@users.noreply.github.com>
2024-04-08 16:41:36 +0530

    cors fix

--- core/settings.py
+++ core/settings.py
@@ -50,10 +50,10 @@ LOGIN_REDIRECT_URL = '/results/convert/'
 
 
 MIDDLEWARE = [
-      'corsheaders.middleware.CorsMiddleware',
 
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+      'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
