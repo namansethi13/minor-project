2740a1a0f5486009d443dbadb01450f046986f9d
stinger13 <93597591+stinger13@users.noreply.github.com>
2024-04-09 23:48:05 +0530

    setting samesite attr of session cookie to none

--- core/settings.py
+++ core/settings.py
@@ -67,7 +67,8 @@ MIDDLEWARE = [
 
 
 ROOT_URLCONF = "core.urls"
-
+SESSION_COOKIE_SAMESITE = 'None'
+# SESSION_COOKIE_SECURE = True
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
