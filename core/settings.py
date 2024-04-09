8872bb8e03db4f3ce40d2a36138794ccd46d8445
stinger13 <93597591+stinger13@users.noreply.github.com>
2024-04-09 21:24:15 +0530

    enabled csrf for admin

--- core/settings.py
+++ core/settings.py
@@ -153,7 +153,7 @@ USE_I18N = True
 USE_TZ = True
 
 CSRF_COOKIE_SECURE = False
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000/"]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
