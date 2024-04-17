80e08a4ab5836c68e3e84d10a8522bc9def652fc
stinger13 <93597591+stinger13@users.noreply.github.com>
2024-04-17 20:20:19 +0530

    changes in normalized

--- results/admin.py
+++ results/admin.py
@@ -3,7 +3,7 @@ from django_api_admin.sites import site as api_admin_site
 
 # Register your models here.
 from .models import *
-# admin.site.register([Result,Subject,Course])
-api_admin_site.register(Result)
-api_admin_site.register(Subject)
-api_admin_site.register(Course)
+admin.site.register([Result,Subject,Course])
+# api_admin_site.register(Result)
+# api_admin_site.register(Subject)
+# api_admin_site.register(Course)
