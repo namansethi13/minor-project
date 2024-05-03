04658f58641e8b3562346a2d7cbd75bc3c41ce5b
stinger13 <93597591+stinger13@users.noreply.github.com>
2024-05-04 00:27:06 +0530

    pushed

--- results/format4.py
+++ results/format4.py
@@ -1,9 +1,3 @@
-class f4:
-    def __init__(self, data):
-        self.data = data
-
-    def format(self):
-        return self.dataimport pandas as pd
 from docx import Document
 from docx.shared import Inches
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
