4a49bba239d8ee601df353bd4f864ae0ec7fba31
stinger13 <93597591+stinger13@users.noreply.github.com>
2024-05-13 00:33:25 +0530

    striping spaces before reading1

--- results/views.py
+++ results/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import json
-
+from io import BytesIO
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
