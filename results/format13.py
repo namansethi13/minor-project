f5837cf2f3839b146c0cab8009f15da7e894fa5a
stinger13 <93597591+stinger13@users.noreply.github.com>
2024-05-17 05:14:24 +0530

    changes

--- results/format13.py
+++ results/format13.py
@@ -7,10 +7,8 @@ from docx.shared import Pt, RGBColor
 import uuid
 import os
 import uuid
-
-
 class f13:
-    def __init__(self, request_data, shift, year, faculty_name, semester, section):
+    def __init__(self,request_data,shift,year,faculty_name,semester,section,is_practical):
         course = request_data["course"]
         self.course = course
         self.shift = shift
@@ -21,12 +19,11 @@ class f13:
         self.df = request_data["df"]
         last_three_columns = self.df.columns[-3:]
         self.df.dropna(inplace=True)
-        self.df.sort_values(
-            by=last_three_columns[-3], ascending=True, inplace=True)
+        self.df.sort_values(by=last_three_columns[-3],ascending=True,inplace=True)
         self.df.insert(0, 'S.No.', range(1, 1 + len(self.df)))
-        print('df:', self.df.columns)
+        print('df:',self.df.columns)
         self.subject_name = " ".join(self.df.columns[-1].split(" ")[:-1])
-        print('subject_name:', self.subject_name)
+        print('subject_name:',self.subject_name)
         self.file_name = str(uuid.uuid4())
         self.faculty_name = faculty_name
         self.shift = shift
@@ -44,11 +41,10 @@ class f13:
             10: 'X'
         }
         self.semester_roman = roman_map[int(semester)]
-        str_enrollment_series = self.df['Enrollment Number'].tolist()
-        print(str_enrollment_series)
+        enrollment_series = self.df.iloc[:,1]
+        str_enrollment_series = f"{int(enrollment_series):011d}"
+        df['Enrollment No.'] = str_enrollment_series
 
-        for i in range(len(str_enrollment_series)):
-            str_enrollment_series[i] = f"{int(str_enrollment_series[i]):110d}"
 
     def write_to_doc(self):
         self.word_file_path = os.path.join(os.path.dirname(
@@ -84,43 +80,47 @@ class f13:
         table = doc.tables[0]
         table.style = 'TableGrid'
         table.cell(0, 0).merge(table.cell(1, 0)).text = "S.No."
-        table.cell(0, 1).merge(table.cell(1, 1)).text = "Enrollment No."
-        table.cell(0, 2).merge(table.cell(1, 2)).text = "Name"
-        table.cell(0, 3).merge(table.cell(0, 5)).text = self.subject_name
-        table.cell(1, 3).text = "Int"
-        table.cell(1, 4).text = "Ext"
-        table.cell(1, 5).text = "Total"
-        sno = list(self.df.iloc[:, 0])
-        enroll_no = list(self.df.iloc[:, 1])
-        name = list(self.df.iloc[:, 2])
-        int_marks = list(self.df.iloc[:, -3])
-        ext_marks = list(self.df.iloc[:, -2])
-        total_marks = list(self.df.iloc[:, -1])
+        table.cell(0, 1).merge(table.cell(1,1)).text = "Enrollment No."
+        table.cell(0, 2).merge(table.cell(1,2)).text = "Name"
+        table.cell(0, 3).merge(table.cell(0,5)).text =  self.subject_name
+        table.cell(1,3).text = "Int"
+        table.cell(1,4).text = "Ext"
+        table.cell(1,5).text = "Total"
+        sno = list(self.df.iloc[:,0])
+        print(sno)
+        enroll_no = list(self.df.iloc[:,1])
+        name = list(self.df.iloc[:,2])
+        int_marks = list(self.df.iloc[:,-3])
+        ext_marks = list(self.df.iloc[:,-2])
+        total_marks = list(self.df.iloc[:,-1])
         for i in range(self.df.shape[0]):
-            table.cell(i+2, 0).text = str(sno[i])
-            table.cell(i+2, 1).text = f"{int(enroll_no[i]):110d}"
-            table.cell(i+2, 2).text = str(name[i])
-            table.cell(i+2, 3).text = str(int_marks[i])
-            table.cell(i+2, 4).text = str(ext_marks[i])
-            table.cell(i+2, 5).text = str(total_marks[i])
+            table.cell(i+2,0).text = str(sno[i])
+            table.cell(i+2,1).text = str(enroll_no[i])
+            table.cell(i+2,2).text = str(name[i])
+            table.cell(i+2,3).text = str(int_marks[i])
+            table.cell(i+2,4).text = str(ext_marks[i])
+            table.cell(i+2,5).text = str(total_marks[i])
+
 
         for row in table.rows:
-            for cell in row.cells:
-                paragraphs = cell.paragraphs
-                for paragraph in paragraphs:
-                    paragraph_format = paragraph.paragraph_format
-                    paragraph_format.space_before = Pt(0)
-                    paragraph_format.space_after = Pt(0)
-                    paragraph_format.line_spacing = 1
-                    paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-                    for run in paragraph.runs:
-                        font = run.font
-                        font.name = 'Times New Roman'
-                        font.size = Pt(11)
-
-                        font.color.rgb = RGBColor(0, 0, 0)
+                    for cell in row.cells:
+                        paragraphs = cell.paragraphs
+                        for paragraph in paragraphs:
+                            paragraph_format = paragraph.paragraph_format
+                            paragraph_format.space_before = Pt(0)
+                            paragraph_format.space_after = Pt(0)
+                            paragraph_format.line_spacing = 1
+                            paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+                            for run in paragraph.runs:
+                                font = run.font
+                                font.name = 'Times New Roman'
+                                font.size = Pt(11)
+                                font.bold = True
+                                font.color.rgb = RGBColor(0, 0, 0)
 
         doc.add_paragraph()
+
+
 
         for line in footer_lines:
             paragraph = doc.add_heading(line)
@@ -139,5 +139,7 @@ class f13:
                     font.underline = True
                 font.color.rgb = RGBColor(0, 0, 0)
 
+        
         doc.save(self.word_file_path)
         return f"{self.file_name}"
+
